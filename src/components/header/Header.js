1a7876f4956921d48c83beb7c4b96237d6f0335e
Viktorhs <victor.hugo.gba@hotmail.com.br>
2022-07-31 15:33:55 -0300

    bonus botão voltar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,24 @@
-import { Link } from "react-router-dom"
+
+import { useLocation, useNavigate} from "react-router-dom"
 import styled from "styled-components"
 
 export default function Header(){
+    let location = useLocation()
+    let navigate = useNavigate()
+    let hist = location.pathname
+    
+   function back(){
+        navigate(-1)
+   }
+
     return(
-        <HeaderBar>
-            <Link to='/'>CINEFLEX</Link>
-        </HeaderBar>
+        <>
+            <HeaderBar>
+                {(hist !== '/') ? <Back onClick={() => back()}><ion-icon name="chevron-back-outline"></ion-icon></Back> : <></>}
+                <h1>CINEFLEX</h1>
+            </HeaderBar>
+        </>
+
     )
 }
 
@@ -18,10 +31,36 @@ const HeaderBar = styled.header `
     height: 67px;
     background: #C3CFD9;
 
-    font-weight: 400;
-    font-size: 34px;
-    line-height: 40px;
-    color:#E8833A ;
-    cursor: default;
 
+
+    h1{
+        font-weight: 400;
+        font-size: 34px;
+        line-height: 40px;
+        color:#E8833A ;
+        cursor: default;
+    }
+
+`
+
+const Back = styled.div`
+    position: absolute;
+    left: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 30px;
+    height: 30px;
+    border: 2px solid #507DA4;
+    border-radius: 20%;
+    color: #E8833A;
+    cursor: pointer;
+    transition: 150ms;
+    font-size: 22px;
+    padding-right: 2px;
+
+    &&:active{
+        filter: brightness(85%);
+        transform: translateY(1px);
+    }
 `
